9dae1127a2552ed95ce252276b8b8dd3d6cdacda
roand <roand@inbox.ru>
2012-09-25 22:21:06 +0000

    Задокументирован проект treexmlmodel

--- src/libs/treexmlmodel/tablexmlproxymodel.h
+++ src/libs/treexmlmodel/tablexmlproxymodel.h
@@ -5,22 +5,44 @@
 #include <QStringList>
 #include "treexmlmodelglobal.h"
 
+//! Класс прокси модель  для XML данных класса TreeXMLModel
+/*! Данный класс предназначен для табличного отображения атрибутов
+    класса TreeXMLModel.Позволяет отображать наследуемые тэги, сортировать
+    и фильтровать данные. \n
+    Пример использования:
+    \code
+    TableXMLProxyModel *m_attrModel = new TableXMLProxyModel();
+    QStringList tags;
+    tags << "Attribute";
+    m_attrModel->setAttributeTags(tags);
+    \endcode
+*/
 class XMLMODELLIB TableXMLProxyModel : public QSortFilterProxyModel
 {
     Q_OBJECT
 public:
+    //! Конструктор модели
     TableXMLProxyModel();
 
+    //! Метод который определяет отображать ли указанную строку
     virtual bool filterAcceptsRow (int row, const QModelIndex &parent) const;
+
+    //! Устанавливает индекс модели, в которой будут отображаться только атрибуты
     void setFilterIndex(const QModelIndex &parent);
+
+    //! Получение индекса модели, в которой будут отображаться только атрибуты
     QModelIndex filterIndex();
 
+    //! Получение тэгов, которые являются атрибутами
     QStringList attributeTags();
+
+     //! Установка тэгов, которые являются атрибутами
     void setAttributeTags(QStringList list);
 private:
+    //! Индекс модели, в котором отображаются только атрибуты
     QModelIndex m_index;
+    //! Список тэгов, которые являются атрибутами
     QStringList m_tags;
-
 };
 
 #endif // TABLEXMLPROXYMODEL_H
