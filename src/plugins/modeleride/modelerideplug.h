c8d3e13a7cb267bae82c93393a0c0e1a03aadd2e
Andrey Rodionov <roand@inbox.ru>
2012-12-26 12:01:44 +0400

    Добавлена обработка команды сохранить

--- src/plugins/modeleride/modelerideplug.h
+++ src/plugins/modeleride/modelerideplug.h
@@ -136,6 +136,9 @@ public slots:
 
     //! Установка отображения состава в дереве классов
     void setShownComp(bool shown);
+
+    //! Обработка изменения модели
+    void dataChange(const QModelIndex & topLeft, const QModelIndex & bottomRight);
 };
 
 #endif
