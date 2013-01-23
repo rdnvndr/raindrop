f958f908f1a63d03fbc435fdd14ad72c764606aa
Andrey Rodionov <roand@inbox.ru>
2013-01-23 15:20:28 +0400

    Сделано сохранение меню

---
+++ src/plugins/mainwindow/menubar.h
@@ -0,0 +1,18 @@
+#ifndef MENUBAR_H
+#define MENUBAR_H
+
+#include <QMenuBar>
+
+class MenuBar : public QMenuBar
+{
+    Q_OBJECT
+public:
+    explicit MenuBar(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // MENUBAR_H
