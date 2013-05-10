07633c6b955388bae11db06a5cc80b86fe7c3b11
Andrey Rodionov <roand@inbox.ru>
2013-05-10 01:46:34 +0000

    Начата реализация описания простых фильтров

---
+++ src/plugins/modeleride/filterwidget.cpp
@@ -0,0 +1,104 @@
+#include "filterwidget.h"
+#include "dbxmlstruct.h"
+#include "xmldelegate.h"
+#include <QHeaderView>
+#include <QMessageBox>
+
+FilterWidget::FilterWidget(QWidget *parent) :
+    QWidget(parent)
+{
+    setupUi(this);
+
+    m_filterModel = new TableXMLProxyModel();
+    QStringList tags;
+    tags << DBFILTERXML::FILTER;
+    m_filterModel->setAttributeTags(tags);
+
+    tableViewFilter->setItemDelegate(new XmlDelegate(this));
+    tableViewFilter->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    connect(toolButtonAdd,SIGNAL(clicked()),this,SLOT(add()));
+    connect(toolButtonDelete,SIGNAL(clicked()),this,SLOT(remove()));
+
+    connect(toolButtonEdit,SIGNAL(clicked()),this,SLOT(edit()));
+    connect(checkBoxInInherited,SIGNAL(clicked(bool)),this,SLOT(showParent(bool)));
+}
+
+FilterWidget::~FilterWidget()
+{
+    delete m_filterModel;
+}
+
+void FilterWidget::setModel(TreeXMLModel *model)
+{
+    m_model = model;
+
+    m_filterModel->setSourceModel(m_model);
+    m_filterModel->setHeaderData(0, Qt::Horizontal, tr("Название фильтра"));
+    m_filterModel->setHeaderData(1, Qt::Horizontal, tr("Описание"));
+    m_filterModel->setHeaderData(2, Qt::Horizontal, tr("Первый класс"));
+    m_filterModel->setHeaderData(3, Qt::Horizontal, tr("Второй класс"));
+    m_filterModel->setHeaderData(4, Qt::Horizontal, tr("Описание прямое"));
+    m_filterModel->setHeaderData(5, Qt::Horizontal, tr("Описание обратное"));
+    m_filterModel->setHeaderData(6, Qt::Horizontal, tr("Индетификатор"));
+    m_filterModel->setColumnCount(7);
+    m_filterModel->setDynamicSortFilter(true);
+
+    tableViewFilter->setSortingEnabled(true);
+    tableViewFilter->setModel(m_filterModel);
+    tableViewFilter->sortByColumn(0,Qt::AscendingOrder);
+    tableViewFilter->setColumnHidden(6,true);
+}
+
+void FilterWidget::add()
+{
+}
+
+void FilterWidget::remove()
+{
+}
+
+void FilterWidget::edit()
+{
+}
+
+void FilterWidget::showParent(bool flag)
+{
+    m_filterModel->setFilterRole(Qt::EditRole);
+
+    if (flag==true){
+        m_filterModel->setFilterRegExp("");
+    } else {
+        QModelIndex index = tableViewFilter->rootIndex();
+        QString className = modelData(DBCLASSXML::CLASS,
+                                      DBCLASSXML::ID,
+                                      index).toString();
+        className.replace("{","\\{");
+        className.replace("}","\\}");
+        if (className.isEmpty()){
+            m_filterModel->setFilterRegExp("\\S*");
+        }else
+            m_filterModel->setFilterRegExp(className);
+    }
+    m_filterModel->setFilterKeyColumn(m_model->columnDisplayedAttr(DBFILTERXML::FILTER,
+                                                                DBFILTERXML::PARENT));
+}
+
+void FilterWidget::setRootIndex(QModelIndex index)
+{
+    QModelIndex rootIndex = m_filterModel->mapToSource(tableViewFilter->rootIndex());
+    if (rootIndex == index)
+        return;
+
+    m_filterModel->setFilterIndex(index);
+    m_filterModel->setSourceModel(m_model);
+
+    tableViewFilter->setRootIndex(m_filterModel->mapFromSource(index));
+    tableViewFilter->setCurrentIndex(tableViewFilter->rootIndex().child(0,0));
+}
+
+QVariant FilterWidget::modelData(const QString &tag, const QString &attr, const QModelIndex &index)
+{
+    return index.sibling(index.row(), m_model->columnDisplayedAttr(
+                             tag,attr)).data();
+}
---
+++ src/plugins/modeleride/filterwidget.h
@@ -0,0 +1,72 @@
+#ifndef FILTERWIDGET_H
+#define FILTERWIDGET_H
+
+#include "ui_filterwidget.h"
+#include <treexmlmodel/tablexmlproxymodel.h>
+#include <treexmlmodel/treexmlmodel.h>
+#include <QDataWidgetMapper>
+
+//! Диалог редактирования фильтров класса
+/*! Диалог предназначен для редактирования списка фильтров
+    класса
+*/
+
+class FilterWidget : public QWidget, private Ui::FilterWidget
+{
+    Q_OBJECT
+    
+public:
+    //! Конструктор редактора фильтров
+    explicit FilterWidget(QWidget *parent = 0);
+
+    //! Деструктор редактора фильтров
+    ~FilterWidget();
+
+    //! Установка модели для редактирования составов класса
+    void setModel(TreeXMLModel *model);
+
+public slots:
+    //! Добавление фильтра класса
+    void add();
+
+    //! Удаление фильтра класса
+    void remove();
+
+    //! Вызов редактирования фильтра класса
+    void edit();
+
+    //! Установка отображения родительских фильтров класса
+    void showParent(bool flag);
+
+    //! Установка родителя фильтра
+    void setRootIndex(QModelIndex index);
+
+signals:
+    //! Сигнал о добавлении данных
+    void dataAdded(const QModelIndex &index);
+
+    //! Сигнал о вызове редактирования данных
+    void dataEdited(const QModelIndex &index);
+
+    //! Сигнал о изменении данных
+    void dataChanged(const QModelIndex &index);
+
+    //! Сигнал о удалении данных
+    void dataRemoved(const QModelIndex &index);
+
+    //! Сигнал о изменении текущего состава
+    void currentIndexChanged(const QModelIndex &index);
+
+private:
+    //! Получение данных модели
+    QVariant modelData(const QString &tag, const QString &attr, const QModelIndex &index);
+
+    //! Прокси модель для фильтров класса
+    TableXMLProxyModel* m_filterModel;
+
+    //! Модель структуры классов
+    TreeXMLModel* m_model;
+
+};
+
+#endif // FILTERWIDGET_H
