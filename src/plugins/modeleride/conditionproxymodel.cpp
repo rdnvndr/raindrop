9376b19806cd4362eca7864dd31b276847ece0ff
Andrey Rodionov <roand@inbox.ru>
2013-05-14 17:43:21 +0400

    Проверка работы proxy model

--- src/plugins/modeleride/conditionproxymodel.cpp
+++ src/plugins/modeleride/conditionproxymodel.cpp
@@ -1,5 +1,47 @@
 #include "conditionproxymodel.h"
 
-ConditionProxyModel::ConditionProxyModel()
+ConditionProxyModel::ConditionProxyModel(QObject *parent) :
+    QAbstractProxyModel(parent)
 {
+}
+int ConditionProxyModel::columnCount(const QModelIndex& parent) const
+{
+     return sourceModel()->columnCount(mapToSource(parent)) ;
+}
+
+int ConditionProxyModel::rowCount(const QModelIndex& parent) const
+{
+    return sourceModel()->rowCount(mapToSource(parent)) ;
+}
+
+QModelIndex ConditionProxyModel::index(int row, int column, const QModelIndex& parent) const
+{
+    if (!hasIndex(row, column, parent))
+        return QModelIndex();
+
+    return mapFromSource(sourceModel()->index(row, column, mapToSource(parent))) ;
+}
+
+QModelIndex ConditionProxyModel::parent(const QModelIndex& index) const
+{
+    if (index.isValid() == false)
+        return QModelIndex();
+
+    return mapFromSource(sourceModel()->parent(mapToSource(index))) ;
+}
+
+QModelIndex ConditionProxyModel::mapFromSource(const QModelIndex& index) const
+{
+    if (index.isValid())
+        return createIndex(index.row(), index.column());
+    else
+        return QModelIndex();
+}
+
+QModelIndex ConditionProxyModel::mapToSource(const QModelIndex& index) const
+{
+    if (index.isValid())
+        return sourceModel()->index(index.row(), index.column(), mapToSource(index.parent())) ;
+    else
+        return QModelIndex();
 }
--- src/plugins/modeleride/conditionproxymodel.h
+++ src/plugins/modeleride/conditionproxymodel.h
@@ -3,10 +3,16 @@
 
 #include <QIdentityProxyModel>
 
-class ConditionProxyModel : public QIdentityProxyModel
+class ConditionProxyModel : public QAbstractProxyModel
 {
 public:
-    ConditionProxyModel();
+    ConditionProxyModel(QObject *parent = 0);
+    int columnCount (const QModelIndex& parent = QModelIndex()) const ;
+    int rowCount(const QModelIndex& parent) const ;
+    QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const ;
+    QModelIndex parent (const QModelIndex& index) const ;
+    QModelIndex mapFromSource(const QModelIndex&) const ;
+    QModelIndex mapToSource(const QModelIndex&) const ;
 };
 
 #endif // CONDITIONPROXYMODEL_H
