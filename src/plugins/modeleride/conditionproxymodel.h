380bfea1cb8419b9b3257bf88e951a20dd83682e
Andrey Rodionov <roand@inbox.ru>
2013-06-14 17:17:18 +0400

    Добавлены иконки для блоков и выражений

--- src/plugins/modeleride/conditionproxymodel.h
+++ src/plugins/modeleride/conditionproxymodel.h
@@ -15,6 +15,7 @@ public:
     QVariant data(const QModelIndex &proxyIndex, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex &proxyIndex, const QVariant &value,
                  int role = Qt::EditRole);
+    bool insertRows(int row, int count, const QModelIndex &parent);
 };
 
 #endif // CONDITIONPROXYMODEL_H
