41e210d8ef93dbbcba1d06468b7ab4afb2bc11c8
Andrey Rodionov <roand@inbox.ru>
2013-11-28 00:16:11 +0000

    Совмещены настройки горячих клавиш и команд

---
+++ src/plugins/mainwindow/actiongrouptreeview.h
@@ -0,0 +1,21 @@
+#ifndef ACTIONGROUPTREEVIEW_H
+#define ACTIONGROUPTREEVIEW_H
+
+#include <QTreeView>
+
+//! Дерево команд
+//! Дерево команд отличается от стандартного дерева поддержкой Drag and Drop
+//!
+class ActionGroupTreeView : public QTreeView
+{
+    Q_OBJECT
+public:
+     //! Конструктор класса
+    explicit ActionGroupTreeView(QWidget *parent = 0);
+
+    //! Действия для создание перетаскивания
+    void startDrag(Qt::DropActions supportedActions);
+
+};
+
+#endif // ACTIONGROUPTREEVIEW_H
