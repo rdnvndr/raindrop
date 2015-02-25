37a49d04197fd19b03d1a980f3d03c76dc16d1ac
Andrey Rodionov <roand@inbox.ru>
2015-02-25 17:17:35 +0300

    Исправлено перемещение позиции

--- src/plugins/modeleride/compositionwidget.cpp
+++ src/plugins/modeleride/compositionwidget.cpp
@@ -228,8 +228,8 @@ void CompositionWidget::up()
             index = tableViewComp->currentIndex().sibling(index.row()-2,0);
         else
             index = index.sibling(0,0);
-        m_model->removeRow(srcIndex.row(),srcIndex.parent());
-        setCurrent(index);
+        if (m_model->removeRow(srcIndex.row(),srcIndex.parent()))
+            setCurrent(index);
     }
 }
 
@@ -245,8 +245,8 @@ void CompositionWidget::down()
                                                        QStringList());
     if (m_model->moveIndex(srcIndex,srcParent,row)) {
         index = tableViewComp->currentIndex().sibling(index.row()+2,0);
-        m_model->removeRow(srcIndex.row(),srcIndex.parent());
-        setCurrent(index);
+        if (m_model->removeRow(srcIndex.row(),srcIndex.parent()))
+            setCurrent(index);
     }
 }
 
