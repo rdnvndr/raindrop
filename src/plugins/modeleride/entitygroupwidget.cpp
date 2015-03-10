839d74ae64460f48f766b39eb522fc7cf1274644
roand <roand@inbox.ru>
2015-03-11 00:08:28 +0300

    EntityGroupWidget переведен на AbstractEditorWidget

---
+++ src/plugins/modeleride/entitygroupwidget.cpp
@@ -0,0 +1,116 @@
+#include "entitygroupwidget.h"
+
+#include <metadatamodel/dbxmlstruct.h>
+#include "xmldelegate.h"
+
+using namespace RTPTechGroup::MetaDataModel;
+
+namespace RTPTechGroup {
+namespace ModelerIde {
+
+EntityGroupWidget::EntityGroupWidget(QWidget *parent) :
+    AbstractEditorWidget(parent)
+{
+    setupUi(this);
+
+    connect(toolButtonAdd,  SIGNAL(clicked()), this, SLOT(add()));
+    connect(toolButtonDel,  SIGNAL(clicked()), this, SLOT(remove()));
+    connect(toolButtonEdit, SIGNAL(clicked()), this, SLOT(edit()));
+
+    connect(pushButtonPropCancel, SIGNAL(clicked()), this, SLOT(revert()));
+    connect(pushButtonPropSave,   SIGNAL(clicked()), this, SLOT(submit()));
+}
+
+EntityGroupWidget::~EntityGroupWidget()
+{
+
+}
+
+void EntityGroupWidget::setModel(TreeXmlHashModel *model)
+{
+    AbstractEditorWidget::setModel(model);
+
+    dataMapper()->addMapping(lineEditEntityGroupName,
+                         model->columnDisplayedAttr(DBENTITYGROUPXML::ENTITYGROUP,
+                                                    DBENTITYXML::NAME));
+    dataMapper()->addMapping(lineEditEntityGroupDesc,
+                         model->columnDisplayedAttr(DBENTITYGROUPXML::ENTITYGROUP,
+                                                   DBENTITYGROUPXML::DESCRIPTION));
+}
+
+bool EntityGroupWidget::isRemove(const QModelIndex &srcIndex)
+{
+    const TreeXmlHashModel *model = dynamic_cast<const TreeXmlHashModel *>(srcIndex.model());
+    if (!model)
+        return false;
+
+    bool success = true;
+    QString msg;
+
+    QStringList tags;
+    tags << DBENTITYXML::ENTITY;
+    if (model->hasChildren(srcIndex,tags)) {
+        msg += tr("Необходимо удалить сущности ЕИ.\n\n");
+        success = false;
+    }
+    if (!success) {
+        QMessageBox msgBox;
+        msgBox.setText(tr("Удаление данного объекта не воможно."));
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.setDetailedText(msg);
+        msgBox.setWindowTitle(tr("Предупреждение"));
+        msgBox.exec();
+    }
+    return success;
+}
+
+bool EntityGroupWidget::isEmpty()
+{
+    return lineEditEntityGroupName->text().isEmpty();
+}
+
+void EntityGroupWidget::add()
+{
+    AbstractEditorWidget::add(DBENTITYGROUPXML::ENTITYGROUP);
+}
+
+void EntityGroupWidget::edit(bool flag)
+{
+    if (groupBoxEntityGroup->isEnabled()==flag)
+        return;
+
+    if (isEmpty()) {
+        toolButtonAdd->setDisabled(true);
+        flag = true;
+    } else
+        toolButtonAdd->setEnabled(true);
+
+    groupBoxEntityGroup->setEnabled(flag);
+    pushButtonPropSave->setEnabled(flag);
+    pushButtonPropCancel->setEnabled(flag);
+    toolButtonEdit->setDisabled(flag);
+
+    emit edited(flag);
+}
+
+void EntityGroupWidget::submit()
+{
+    QModelIndex existIndex = model()->indexHashAttr(
+                DBENTITYGROUPXML::ENTITYGROUP,
+                DBENTITYGROUPXML::NAME,
+                lineEditEntityGroupName->text());
+    QModelIndex srcIndex = model()->index(dataMapper()->currentIndex(),0,
+                                          dataMapper()->rootIndex());
+
+    if (existIndex.isValid()){
+        if (existIndex.sibling(existIndex.row(),0)!=srcIndex){
+            QMessageBox::warning(this,tr("Предупреждение"),
+                                 tr("Группа сущность ЕИ с таким именем уже существует"));
+            return;
+        }
+    }
+
+    AbstractEditorWidget::submit();
+}
+
+}}
--- src/plugins/modeleride/propentitygroup.cpp
+++ src/plugins/modeleride/propentitygroup.cpp
@@ -15,131 +15,31 @@ PropEntityGroup::PropEntityGroup(QWidget *parent) :
 {
     setupUi(this);
 
-    connect(this,SIGNAL(currentIndexChanged(QModelIndex)),
+    connect(entityGroupWidget,SIGNAL(currentIndexChanged(QModelIndex)),
             this,SLOT(setTabName(QModelIndex)));
-    connect(this,SIGNAL(dataChanged(QModelIndex)),
+    connect(entityGroupWidget,SIGNAL(dataChanged(QModelIndex)),
             this,SLOT(setTabName(QModelIndex)));
-    connect(this,SIGNAL(dataRemoved(QModelIndex)),
+    connect(entityGroupWidget,SIGNAL(dataRemoved(QModelIndex)),
             this,SLOT(closeTab(QModelIndex)));
-
-
-    connect(toolButtonAdd,  SIGNAL(clicked()), this, SLOT(add()));
-    connect(toolButtonDel,  SIGNAL(clicked()), this, SLOT(remove()));
-    connect(toolButtonEdit, SIGNAL(clicked()), this, SLOT(edit()));
-
-    connect(pushButtonPropCancel, SIGNAL(clicked()), this, SLOT(revert()));
-    connect(pushButtonPropSave,   SIGNAL(clicked()), this, SLOT(submit()));
-
-    m_mapper = new QDataWidgetMapper();
-    m_mapper->setItemDelegate(new XmlDelegate(this));
-    m_mapper->setSubmitPolicy(QDataWidgetMapper::ManualSubmit);
-
-    m_oldIndex = QModelIndex();
 }
 
 PropEntityGroup::~PropEntityGroup()
 {
-    delete m_mapper;
+
 }
 
 void PropEntityGroup::setModel(TreeXmlHashModel *model)
 {
+    entityGroupWidget->setModel(model);
+
     AbstractPropEditor::setModel(model);
-
-    connect(model,SIGNAL(rowsRemoved(QModelIndex,int,int)),
-            this,SLOT(rowsRemoved(QModelIndex,int,int)));
-    m_mapper->setModel(model);
-
-    m_mapper->addMapping(lineEditEntityGroupName,
-                         model->columnDisplayedAttr(DBENTITYGROUPXML::ENTITYGROUP,
-                                                    DBENTITYXML::NAME));
-    m_mapper->addMapping(lineEditEntityGroupDesc,
-                         model->columnDisplayedAttr(DBENTITYGROUPXML::ENTITYGROUP,
-                                                   DBENTITYGROUPXML::DESCRIPTION));
 }
 
-bool PropEntityGroup::isRemove(const QModelIndex &srcIndex)
-{
-    const TreeXmlHashModel *model = dynamic_cast<const TreeXmlHashModel *>(srcIndex.model());
-    if (!model)
-        return false;
 
-    bool success = true;
-    QString msg;
-
-    QStringList tags;
-    tags << DBENTITYXML::ENTITY;
-    if (model->hasChildren(srcIndex,tags)) {
-        msg += tr("Необходимо удалить сущности ЕИ.\n\n");
-        success = false;
-    }
-    if (!success) {
-        QMessageBox msgBox;
-        msgBox.setText(tr("Удаление данного объекта не воможно."));
-        msgBox.setIcon(QMessageBox::Warning);
-        msgBox.setDetailedText(msg);
-        msgBox.setWindowTitle(tr("Предупреждение"));
-        msgBox.exec();
-    }
-    return success;
-}
-
-bool PropEntityGroup::isEmpty()
-{
-    return lineEditEntityGroupName->text().isEmpty();
-}
-
-void PropEntityGroup::add()
-{
-    m_oldIndex =  model()->index(m_mapper->currentIndex(),
-                                 0,m_mapper->rootIndex());
-    QModelIndex srcIndex = m_oldIndex.parent();
-    QModelIndex srcCurrentIndex =
-            model()->insertLastRows(0,1,srcIndex,DBENTITYGROUPXML::ENTITYGROUP);
-    if (srcCurrentIndex.isValid()) {
-        setCurrent(srcCurrentIndex);
-        edit(true);
-    }
-}
-
-void PropEntityGroup::remove()
-{
-    QModelIndex srcIndex = model()->index(m_mapper->currentIndex(),0,m_mapper->rootIndex());
-
-    if (!isRemove(srcIndex))
-        return;
-
-    m_mapper->revert();
-    setCurrent(srcIndex.parent());
-
-    model()->removeRow(srcIndex.row(),srcIndex.parent());
-    emit dataRemoved(srcIndex);
-}
-
-bool PropEntityGroup::removeEmpty()
-{
-    if (isEmpty()){
-        if (m_oldIndex.isValid()){
-            QModelIndex srcIndex = model()->index(m_mapper->currentIndex(),0,m_mapper->rootIndex());
-            m_mapper->revert();
-            setCurrent(m_oldIndex);
-            model()->removeRow(srcIndex.row(),srcIndex.parent());
-            m_oldIndex = QModelIndex();
-        }else {
-            remove();        
-        }
-        return true;
-    }
-    return false;
-}
 
 void PropEntityGroup::setCurrent(const QModelIndex &index)
 {
-    m_mapper->setRootIndex(index.parent());
-    emit currentIndexChanged(index);
-    m_mapper->setCurrentModelIndex(index);
-
-    edit(false);
+    entityGroupWidget->setCurrent(index);
 }
 
 void PropEntityGroup::setTabName(const QModelIndex &index)
@@ -160,61 +60,7 @@ void PropEntityGroup::setTabName(const QModelIndex &index)
 
 void PropEntityGroup::edit(bool flag)
 {
-
-    if (groupBoxEntityGroup->isEnabled()==flag)
-        return;
-
-    if (isEmpty()) {
-        toolButtonAdd->setDisabled(true);
-        flag = true;
-    } else
-        toolButtonAdd->setEnabled(true);
-
-    groupBoxEntityGroup->setEnabled(flag);
-    pushButtonPropSave->setEnabled(flag);
-    pushButtonPropCancel->setEnabled(flag);
-    toolButtonEdit->setDisabled(flag);
-
-    emit edited(flag);
-}
-
-void PropEntityGroup::submit()
-{
-    QModelIndex existIndex = model()->indexHashAttr(
-                DBENTITYGROUPXML::ENTITYGROUP,
-                DBENTITYGROUPXML::NAME,
-                lineEditEntityGroupName->text());
-    QModelIndex srcIndex = model()->index(m_mapper->currentIndex(),0,m_mapper->rootIndex());
-
-    if (existIndex.isValid()){
-        if (existIndex.sibling(existIndex.row(),0)!=srcIndex){
-            QMessageBox::warning(this,tr("Предупреждение"),
-                                 tr("Группа сущность ЕИ с таким именем уже существует"));
-            return;
-        }
-    }
-
-    m_mapper->submit();
-    edit(false);
-    if (!removeEmpty())
-        emit dataChanged(srcIndex);
-}
-
-void PropEntityGroup::revert()
-{
-    m_mapper->revert();
-    QModelIndex srcIndex = model()->index(m_mapper->currentIndex(),0,m_mapper->rootIndex());
-    setCurrent(srcIndex);
-    edit(false);
-    removeEmpty();
-}
-
-void PropEntityGroup::rowsRemoved(const QModelIndex &index, int start, int end)
-{
-    Q_UNUSED (start)
-    Q_UNUSED (end)
-    if (index == m_mapper->rootIndex() && m_mapper->currentIndex()==-1)
-        emit dataRemoved(QModelIndex());
+    entityGroupWidget->edit(flag);
 }
 
 }}
