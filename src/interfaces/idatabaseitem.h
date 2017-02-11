5130af50f128405bd78d449aba93a4ef5e8a78c2
Andrey Rodionov <roand@inbox.ru>
2017-02-11 19:12:47 +0300

    Добавлено соединение БД в IDatabaseItem

--- src/interfaces/idatabaseitem.h
+++ src/interfaces/idatabaseitem.h
@@ -3,27 +3,30 @@
 
 #include <QUuid>
 #include <QString>
+#include <QSqlDatabase>
 
 //! Элемент базы данных
 class IDatabaseItem
 {
+
+public:
     //! Возращает идентификатор элемента базы данных
-    virtual QUuid id() { return m_id; };
+    virtual QUuid id() { return m_id; }
 
     //! Устанавливает идентификатор элемента базы данных
-    virtual void setId(QUuid id) { m_id = id; };
+    virtual void setId(QUuid id) { m_id = id; }
 
     //! Возращает имя элемента базы данных
-    virtual QString name() {return m_name; };
+    virtual QString name() {return m_name; }
 
     //! Устанавливает имя элемента базы данных
-    virtual void setName(const QString &name) { m_name = name; };
+    virtual void setName(const QString &name) { m_name = name; }
 
     //! Возращает псевдоним элемента базы данных
-    virtual QString alias() { return m_alias; };
+    virtual QString alias() { return m_alias; }
 
     //! Устанавливает псевдоним элемента базы данных
-    virtual void setAlias(const QString &alias) { m_alias = alias; };
+    virtual void setAlias(const QString &alias) { m_alias = alias; }
 
     //! Проверка существования элемента базы данных
     virtual bool isExist() = 0;
@@ -37,7 +40,17 @@ class IDatabaseItem
     //! Удаление элемента базы данных
     virtual bool remove() = 0;
 
+    //! Получения соединения с базой данных
+    virtual QSqlDatabase database() const { return m_database; }
+
+    //! Установка соединения с базой данных
+    virtual void setDatabase(const QSqlDatabase &database)
+    { m_database = database; }
+
 private:
+    //! Соединение с базой данных
+    QSqlDatabase m_database;
+
     //! Индетификатор элемента базы данных
     QUuid m_id;
 
