b7b3e6d4930370d8d6789138367497b99653c6b7
roand <roand@inbox.ru>
2017-02-23 07:04:06 +0300

    Рефакторинг плагина DbConnect

--- src/plugins/dbconnect/dbconnect.cpp
+++ src/plugins/dbconnect/dbconnect.cpp
@@ -20,7 +20,7 @@ DbConnect::DbConnect(QObject *parent):
     actionDbConnect = new QAction(QIcon(":connect"), tr("Соединение с  БД"), this);
     connect(actionDbConnect, &QAction::triggered, this, &DbConnect::dbConnect);
     actionDbConnect->setObjectName("actionDbConnect");
-    iMainWindow->addAction(tr("Работа с БД"),actionDbConnect);
+    iMainWindow->addAction(tr("Работа с БД"), actionDbConnect);
 }
 
 DbConnect::~DbConnect()
@@ -40,72 +40,83 @@ void DbConnect::dbConnect()
 
     DialogConnect *windowConnect = new DialogConnect(
                 qobject_cast<QWidget *>(iMainWindow->instance()));
+
     settings()->beginGroup("DbConnect");
-    windowConnect->comboDriver->setCurrentIndex(
-                windowConnect->comboDriver->findText(settings()->value("driver").toString()));
-    windowConnect->editDatabase->setText(settings()->value("database").toString());
-    windowConnect->editHostname->setText(settings()->value("hostname").toString());
-    windowConnect->portSpinBox->setValue(settings()->value("port",-1).toInt());
-    windowConnect->editUsername->setText(settings()->value("username").toString());
+    windowConnect->setDriver(settings()->value("driver").toString());
+    windowConnect->setDatabaseName(settings()->value("database").toString());
+    windowConnect->setHostName(settings()->value("hostname").toString());
+    windowConnect->setPort(settings()->value("port",-1).toInt());
+    windowConnect->setUserName(settings()->value("username").toString());
+
     while (true) {
-        if (windowConnect->exec() == QDialog::Accepted) {
-            if (QSqlDatabase::database().isOpen()) {
-                QSqlDatabase::database().close();
-                QSqlDatabase::removeDatabase(QSqlDatabase::defaultConnection);
-            }
-            {
-                QSqlDatabase db;
+        if (windowConnect->exec() != QDialog::Accepted)
+            break;
 
-
-                if (windowConnect->comboDriver->currentText()=="MSSQL"){
-                    db = QSqlDatabase::addDatabase("QODBC");
-                    db.setDatabaseName(QString("%1;%2;%3").arg("DRIVER={SQL Server}").
-                                       arg("DATABASE="+windowConnect->editDatabase->text()).
-                                       arg("SERVER="+windowConnect->editHostname->text())
-                                       );
-                    db.setPort(windowConnect->portSpinBox->value());
-                    db.setUserName(windowConnect->editUsername->text());
-                    db.setPassword(windowConnect->editPassword->text());
-                } else if (windowConnect->comboDriver->currentText()=="QOCI"){
-                    db = QSqlDatabase::addDatabase("QOCI");
-
-                    db.setDatabaseName(QString("(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = %1)(PORT = %2)))(CONNECT_DATA =(SERVER = DEDICATED)(SID =%3)))").
-                                       arg(windowConnect->editHostname->text()).
-                                       arg(windowConnect->portSpinBox->value()).
-                                       arg(windowConnect->editDatabase->text())
-                                       );
-                }
-                else {
-                    db = QSqlDatabase::addDatabase(windowConnect->comboDriver->currentText());
-                    db.setDatabaseName(windowConnect->editDatabase->text());
-                    db.setHostName(windowConnect->editHostname->text());
-                }
-
-                db.setPort(windowConnect->portSpinBox->value());
-                db.setUserName(windowConnect->editUsername->text());
-                db.setPassword(windowConnect->editPassword->text());
-
-                if (!db.open()) {
-                    QSqlError err = db.lastError();
-                    if (err.type() != QSqlError::NoError)
-                        QMessageBox::warning(NULL, tr("Не удается открыть базу данных"), tr("Произошла ошибка при создании соединения: ") + err.text());
-                }
-                else {
-                    // OK
-                    settings()->setValue("driver", windowConnect->comboDriver->currentText());
-                    settings()->setValue("database", windowConnect->editDatabase->text());
-                    settings()->setValue("hostname", windowConnect->editHostname->text());
-                    settings()->setValue("port", windowConnect->portSpinBox->value());
-                    settings()->setValue("username", windowConnect->editUsername->text());
-                    break;
-                }
-            }
+        if (QSqlDatabase::database().isOpen()) {
+            QSqlDatabase::database().close();
             QSqlDatabase::removeDatabase(QSqlDatabase::defaultConnection);
         }
-        else {
-            // NOT OK
-            settings()->endGroup();
-            return;
+
+        {
+            QSqlDatabase db;
+
+            if (windowConnect->driver() == "MSSQL"){
+                db = QSqlDatabase::addDatabase("QODBC");
+
+                db.setDatabaseName(
+                            QString("%1;%2;%3").arg("DRIVER={SQL Server}")
+                            .arg("DATABASE="+windowConnect->databaseName())
+                            .arg("SERVER="+windowConnect->hostName()));
+                db.setPort(windowConnect->port());
+                db.setUserName(windowConnect->userName());
+                db.setPassword(windowConnect->password());
+            } else if (windowConnect->driver() == "QOCI") {
+                db = QSqlDatabase::addDatabase("QOCI");
+
+                db.setDatabaseName(
+                            QString("(DESCRIPTION = "
+                                        "(ADDRESS_LIST = "
+                                            "(ADDRESS = "
+                                                "(PROTOCOL = TCP)"
+                                                "(HOST = %1)"
+                                                "(PORT = %2)"
+                                            ")"
+                                        ")"
+                                        "(CONNECT_DATA ="
+                                            "(SERVER = DEDICATED)"
+                                            "(SID =%3)"
+                                        ")"
+                                    ")")
+                            .arg(windowConnect->hostName())
+                            .arg(windowConnect->port())
+                            .arg(windowConnect->databaseName()));
+            } else {
+                db = QSqlDatabase::addDatabase(windowConnect->driver());
+                db.setDatabaseName(windowConnect->databaseName());
+                db.setHostName(windowConnect->hostName());
+            }
+
+            db.setPort(windowConnect->port());
+            db.setUserName(windowConnect->userName());
+            db.setPassword(windowConnect->password());
+
+            if (!db.open()) {
+                QSqlError err = db.lastError();
+                if (err.type() != QSqlError::NoError)
+                    QMessageBox::warning(
+                                NULL,
+                                tr("Не удается открыть базу данных"),
+                                tr("Произошла ошибка при создании соединения:\n")
+                                + err.text());
+                QSqlDatabase::removeDatabase(QSqlDatabase::defaultConnection);
+            } else {
+                settings()->setValue("driver",   windowConnect->driver());
+                settings()->setValue("database", windowConnect->databaseName());
+                settings()->setValue("hostname", windowConnect->hostName());
+                settings()->setValue("port",     windowConnect->port());
+                settings()->setValue("username", windowConnect->userName());
+                break;
+            }
         }
     }
     settings()->endGroup();
--- src/plugins/dbconnect/dialogconnect.h
+++ src/plugins/dbconnect/dialogconnect.h
@@ -7,15 +7,52 @@ namespace RTPTechGroup {
 namespace DbConnect {
 
 //! Диалоговое окно подключения к БД
-class DialogConnect: public QDialog, public Ui::DialogConnect{
+class DialogConnect: public QDialog, private Ui::DialogConnect{
 
-		Q_OBJECT
-	public:
-        //! Коструктор диалогового окна подключения к БД
-        explicit DialogConnect(QWidget *pwgt = 0);
-	public slots:
-        //! Слот для отображения дополнительных параметров подключения к БД
-        void onClickButtonProp();
+    Q_OBJECT
+public:
+    //! Коструктор диалогового окна подключения к БД
+    explicit DialogConnect(QWidget *pwgt = 0);
+
+    //! Установка драйвера БД
+    void setDriver(const QString &name);
+
+    //! Установка имени БД
+    void setDatabaseName(const QString &name);
+
+    //! Установка имени хоста
+    void setHostName(const QString &host);
+
+    //! Установка имени пользователя
+    void setUserName(const QString &name);
+
+    //! Установка пароля
+    void setPassword(const QString &password);
+
+    //! Установка порта
+    void setPort(int port);
+
+    //! Получение драйвера БД
+    QString driver() const;
+
+    //! Получение имени БД
+    QString databaseName() const;
+
+    //! Получение имени хоста
+    QString hostName() const;
+
+    //! Получение имени пользователя
+    QString userName() const;
+
+    //! Получение пароля
+    QString password() const;
+
+    //! Получение порта
+    int port() const;
+
+public slots:
+    //! Слот для отображения дополнительных параметров подключения к БД
+    void onClickButtonProp();
 };
 
 }}
