0df226d24379e2114b325069d5d58382edb1f6d5
Andrey Rodionov <roand@inbox.ru>
2017-07-10 21:25:13 +0300

    Добавлено логирование в PushButtonImage

---
+++ src/libs/pushbuttonimage/pushbuttonimage_p.h
@@ -0,0 +1,14 @@
+#ifndef PUSHBUTTONIMAGE_P_H
+#define PUSHBUTTONIMAGE_P_H
+
+#include <QtCore/QLoggingCategory>
+
+namespace RTPTechGroup {
+namespace Widgets {
+
+Q_DECLARE_LOGGING_CATEGORY(lcWidgets)
+Q_LOGGING_CATEGORY(lcPlugin,"RTPTechGroup.Widgets")
+
+}}
+
+#endif // PUSHBUTTONIMAGE_P_H
