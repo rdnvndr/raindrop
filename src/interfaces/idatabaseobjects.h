1e39aa42296f50eb1e491ea2cd582da21e6485ed
Andrey Rodionov <roand@inbox.ru>
2017-08-06 23:48:36 +0300

    Добавлены методы работы со списком объектов

--- src/interfaces/idatabaseobjects.h
+++ src/interfaces/idatabaseobjects.h
@@ -3,11 +3,15 @@
 
 #include <idatabaseexpression.h>
 #include <idatabaselist.h>
+#include <idatabaseattribute.h>
 
 //! Список объектов базы данных
 class IDatabaseObjects: public IDatabaseList<IDatabaseClass, IDatabaseObject>
 {
 public:
+    //! Перечисление типов сцепления
+    enum JoinType {Inner, LeftOuter, RightOuter, FullOuter, Cross};
+
     //! Конструктор класса
     explicit IDatabaseObjects(IDatabaseClass *item)
         :IDatabaseList<IDatabaseClass, IDatabaseObject>(item) {}
@@ -22,29 +26,49 @@ public:
     //! Получение отфильтрованных объектов класса
     virtual IDatabaseObjects *filtering(IDatabaseExpression *expr) = 0;
 
-    //! Получение всех объектов класса
-    virtual IDatabaseObjects *all() = 0;
 
-    //! Получение отсортированных объектов
-    virtual IDatabaseObjects *orderBy() = 0;
+    //! Получение отсортированных объектов по возрастанию
+    virtual IDatabaseObjects *orderByAsc(IDatabaseAttribute *attr, ...) = 0;
+
+    //! Получение отсортированных объектов по убыванию
+    virtual IDatabaseObjects *orderByDesc(IDatabaseAttribute *attr, ...) = 0;
 
     //! Получение уникальных объектов
-    virtual IDatabaseObjects *distinct() = 0;
-
-    //! Сцепление объектов
-    virtual IDatabaseObjects *join() = 0;
-
-    //! Объединение объектов
-    virtual IDatabaseObjects *unionAll() = 0;
+    virtual IDatabaseObjects *distinct(IDatabaseAttribute *attr, ...) = 0;
 
     //! Ограничение объектов
     virtual IDatabaseObjects *limit(int up, int down = 0) = 0;
 
     //! Ограничение списка возращаемых полей
-    virtual IDatabaseObjects *values() = 0;
+    virtual IDatabaseObjects *values(IDatabaseAttribute *attr, ...) = 0;
+
+
+    //! Сцепление объектов
+    virtual IDatabaseObjects *join(IDatabaseFilter *filter,
+                                   JoinType joinType = Inner) = 0;
+
+    //! Сцепление объектов
+    virtual IDatabaseObjects *join(IDatabaseExpression *expr,
+                                   JoinType joinType = Inner) = 0;
+
+    //! Объединение объектов
+    virtual IDatabaseObjects *unionAll(IDatabaseObjects *objs) = 0;
+
 
     //! Получение количества объектов
-    virtual int count() = 0;
+    virtual int count(IDatabaseAttribute *attr=NULL, bool distinct=false) = 0;
+
+    //! Получение среднего значения атрибута
+    virtual int avg(IDatabaseAttribute *attr) = 0;
+
+    //! Получение минимального значения атрибута
+    virtual int min(IDatabaseAttribute *attr) = 0;
+
+    //! Получение максимального значения атрибута
+    virtual int max(IDatabaseAttribute *attr) = 0;
+
+    //! Получение суммарного значения атрибута
+    virtual int sum(IDatabaseAttribute *attr) = 0;
 
 };
 
